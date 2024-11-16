915f1e7b8f4e60cb3fe361438cb6eff7136bffc0
r-happy <hr325913@gmail.com>
2024-11-16 15:04:05 +0900

    add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="h-[340px] w-full flex justify-center items-center">
+            <p className="text-lg font-semibold">loading ...</p>
+        </div>
+    )
+}
